996a6dbb2767c24e2d0439a11eee93b62dd4df1b
skabayaraa <skabayaraa208@gmail.com>
2026-02-11 16:19:32 +0800

    uurchilsun

--- src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,19 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape s1 = new Square(2);
+        Shape s2 = new Rectangle(2, 3);
 
-        Renderer renderer = new Renderer(rectangle);
 
-        renderer.draw();
+        Renderer r1 = new Renderer(s1);
+        Renderer r2 = new Renderer(s2);
+
+        r1.draw();
+        r2.draw();
     }
 }
